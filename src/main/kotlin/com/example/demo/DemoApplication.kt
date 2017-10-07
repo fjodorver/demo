2e32a224199d0fc57131911afd26891fc22eae35
Fjodor Versinin <fjodor@vershinin.net>
2017-10-07 15:48:17 +0300

    fix getting user

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore
 import javax.sql.DataSource
@@ -20,9 +19,7 @@ class DemoApplication {
 
     @Bean
     fun userService(userRepository: UserRepository) = UserDetailsService {
-        userRepository.findById(it)
-                .orElseThrow { UsernameNotFoundException(it) }
-                .toAuthUser()
+        userRepository.findOne(it).toAuthUser()
     }
 
     @Bean
