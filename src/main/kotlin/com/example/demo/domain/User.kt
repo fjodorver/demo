3281f9b2d15d8d1b66423134fd153ba0cecdf1f2
Fjodor Versinin <fjodor@vershinin.net>
2017-10-07 19:46:27 +0300

    fix entity

--- src/main/kotlin/com/example/demo/domain/User.kt
+++ src/main/kotlin/com/example/demo/domain/User.kt
@@ -2,22 +2,24 @@ package com.example.demo.domain
 
 import org.springframework.security.core.userdetails.UserDetails
 import java.io.Serializable
-import java.time.LocalDateTime
+import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 import org.springframework.security.core.userdetails.User as AuthUser
 
 @Entity
+@Table(name = "\"user\"")
 data class User(
         @Id
-        val username: String,
+        val email: String,
         val password: String,
-        val birthday: LocalDateTime,
+        val birthday: LocalDate,
         val isEnabled: Boolean = false
 ) : Serializable
 
 fun User.toAuthUser(): UserDetails = AuthUser
-        .withUsername(username)
+        .withUsername(email)
         .password(password)
         .disabled(!isEnabled)
         .build()
