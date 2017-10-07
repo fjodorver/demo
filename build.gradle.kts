4e1717ea46d20aed3363011a1fc38c19949ebb2f
Fjodor Versinin <fjodor@vershinin.net>
2017-10-07 15:37:42 +0300

    refactored build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.gradle.kotlin.dsl.*
 
 plugins {
     `embedded-kotlin`
-    kotlin(module = "plugin.spring")
-    kotlin(module = "plugin.jpa")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
     id("io.spring.dependency-management") version "1.0.3.RELEASE"
     id("org.springframework.boot") version "1.5.7.RELEASE"
 }
@@ -13,12 +13,12 @@ repositories {
 }
 
 dependencies {
-    implementation(group = "org.springframework.boot", name = "spring-boot-starter-web")
-    implementation(group = "org.springframework.boot", name = "spring-boot-starter-data-jpa")
-    implementation(group = "org.springframework.boot", name = "spring-boot-starter-security")
-    implementation(group = "org.springframework.security.oauth", name = "spring-security-oauth2")
-    implementation(kotlin(module = "stdlib"))
-    implementation(kotlin(module = "reflect"))
-    runtime(group = "org.springframework.boot", name = "spring-boot-devtools")
-    runtime(group = "org.postgresql", name = "postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security.oauth:spring-security-oauth2")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    runtime("org.springframework.boot:spring-boot-devtools")
+    runtime("org.postgresql:postgresql")
 }
