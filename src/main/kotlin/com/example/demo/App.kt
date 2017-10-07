a50b3acb7e65255379b8da0b0d9197914cfc8db3
Fjodor Versinin <fjodor@vershinin.net>
2017-10-07 19:46:12 +0300

    add Jsr310JpaConverters

--- src/main/kotlin/com/example/demo/App.kt
+++ src/main/kotlin/com/example/demo/App.kt
@@ -4,10 +4,13 @@ import com.example.demo.domain.toAuthUser
 import com.example.demo.repository.UserRepository
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters
 import org.springframework.security.core.userdetails.UserDetailsService
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = arrayOf(App::class, Jsr310JpaConverters::class))
 class App {
 
     @Bean
