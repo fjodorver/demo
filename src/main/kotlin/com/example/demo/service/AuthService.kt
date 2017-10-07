1ee5e49693e94e3da28bf7a52b8651c602eafb27
Fjodor Versinin <fjodor@vershinin.net>
2017-10-07 15:51:07 +0300

    use consumer token services

--- src/main/kotlin/com/example/demo/service/AuthService.kt
+++ src/main/kotlin/com/example/demo/service/AuthService.kt
@@ -8,14 +8,16 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices
 import org.springframework.stereotype.Service
 
 @Service
-class AuthService(private val tokenServices: ConsumerTokenServices, private val userRepository: UserRepository) {
+class AuthService(tokenServices: ConsumerTokenServices, private val userRepository: UserRepository) {
+
+    private val tokenServices = tokenServices as DefaultTokenServices
 
     fun signUp(user: User){
         userRepository.save(user)
     }
 
     fun signOut(auth: OAuth2Authentication): Boolean {
-        val accessToken = (tokenServices as DefaultTokenServices).getAccessToken(auth).value
+        val accessToken = tokenServices.getAccessToken(auth).value
         return tokenServices.revokeToken(accessToken)
     }
 }
