0389f914f9d0bbd73eaa45449f9a3c0d1801d60b
Fjodor Versinin <fjodor@vershinin.net>
2017-10-07 15:35:12 +0300

    downgrade to spring-boot 1.5.7.RELEASE && switch to embedded-kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,24 @@
 import org.gradle.kotlin.dsl.*
 
-buildscript {
-    repositories {
-        mavenCentral()
-        maven(url = "https://repo.spring.io/milestone")
-        maven(url = "https://repo.spring.io/snapshot")
-        jcenter()
-    }
-    dependencies {
-        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.M4")
-    }
-}
-
-apply {
-    plugin("org.springframework.boot")
-    plugin("io.spring.dependency-management")
-}
-
 plugins {
-    val kotlinVersion = "1.1.51"
-    kotlin(module = "jvm", version = kotlinVersion)
-    kotlin(module = "plugin.spring", version = kotlinVersion)
-    kotlin(module = "plugin.jpa", version = kotlinVersion)
+    `embedded-kotlin`
+    kotlin(module = "plugin.spring")
+    kotlin(module = "plugin.jpa")
     id("io.spring.dependency-management") version "1.0.3.RELEASE"
+    id("org.springframework.boot") version "1.5.7.RELEASE"
 }
 
 repositories {
-    mavenCentral()
-    maven(url = "https://repo.spring.io/milestone")
-    maven(url = "https://repo.spring.io/snapshot")
     jcenter()
 }
 
 dependencies {
-    val kotlinVersion = "1.1.51"
     implementation(group = "org.springframework.boot", name = "spring-boot-starter-web")
     implementation(group = "org.springframework.boot", name = "spring-boot-starter-data-jpa")
     implementation(group = "org.springframework.boot", name = "spring-boot-starter-security")
     implementation(group = "org.springframework.security.oauth", name = "spring-security-oauth2")
-    implementation(kotlin(module = "stdlib", version = kotlinVersion))
-    implementation(kotlin(module = "reflect", version = kotlinVersion))
+    implementation(kotlin(module = "stdlib"))
+    implementation(kotlin(module = "reflect"))
     runtime(group = "org.springframework.boot", name = "spring-boot-devtools")
-    runtime(group = "com.h2database", name = "h2")
+    runtime(group = "org.postgresql", name = "postgresql")
 }
