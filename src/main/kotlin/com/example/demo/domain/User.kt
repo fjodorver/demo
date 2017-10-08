95ccf2492cb0651e6a73f2c713fe9af6130945ef
Fjodor Versinin <fjodor@vershinin.net>
2017-10-08 10:04:40 +0300

    refactored user entity: add property id && overrided equals and hashcode

--- src/main/kotlin/com/example/demo/domain/User.kt
+++ src/main/kotlin/com/example/demo/domain/User.kt
@@ -3,20 +3,30 @@ package com.example.demo.domain
 import org.springframework.security.core.userdetails.UserDetails
 import java.io.Serializable
 import java.time.LocalDate
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 import org.springframework.security.core.userdetails.User as AuthUser
 
 @Entity
 @Table(name = "\"user\"")
 data class User(
         @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Int? = null,
         val email: String,
         val password: String,
         val birthday: LocalDate,
-        val isEnabled: Boolean = false
-) : Serializable
+        val isEnabled: Boolean = false,
+        val avatar: ByteArray? = null
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return id == (other as User).id
+    }
+
+    override fun hashCode(): Int = id ?: 0
+}
 
 fun User.toAuthUser(): UserDetails = AuthUser
         .withUsername(email)
